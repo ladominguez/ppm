cf20d45164836527ea85e24a35efb9cc91c4a3f9
Luis Antonio <ladominguez@ucla.edu>
2020-12-27 17:50:50 -0600

    create_chart.py updated.

--- create_chart.py
+++ create_chart.py
@@ -4,16 +4,23 @@ import matplotlib.pyplot as plt
 import sqlite3
 import pandas
 import matplotlib.dates as md
+plt.rcParams['timezone'] = 'Mexico/General'
+
+cmd_sql = r"select  *,datetime(ts,'unixepoch','localtime')  from measurements2 where ts >= (select strftime('%s',(select datetime('now','-8 hour','localtime'))))"
 
 db = sqlite3.connect("air_quality.db")
-df = pandas.read_sql_query("select ts, m1_0_cf1, m2_5_cf1, m10_0_cf1 from measurements2 order by ts", db)
+df = pandas.read_sql_query(cmd_sql, db)
 df2 = df
 
-df2['m1_0_cf1'] = df2['m1_0_cf1'].clip(lower=10)
-df2['m2_5_cf1'] = df2['m2_5_cf1'].clip(lower=10)
-df2['m10_0_cf1'] = df2['m10_0_cf1'].clip(lower=10)
+df2['m1_0_cf1'] = df2['m1_0_cf1'].clip(upper=500)
+df2['m2_5_cf1'] = df2['m2_5_cf1'].clip(upper=500)
+df2['m10_0_cf1'] = df2['m10_0_cf1'].clip(upper=500)
 df2['ts'] = pandas.to_datetime(df2['ts'], unit='s')
 
-df2.plot(x='ts')
+df2.plot(x='ts',y=['m1_0_cf1', 'm2_5_cf1', 'm10_0_cf1'])
+date_form = md.DateFormatter("%y/%m/%d %H:%M")
+plt.gca().xaxis.set_major_formatter(date_form)
+plt.grid()
 plt.savefig('ppm.png')
+
 
